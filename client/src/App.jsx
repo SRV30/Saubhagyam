9865254566cc09b01335a3cf3ee702f36f190ab5
SRV30 <sahilrv196@gmail.com>
2026-05-15 22:31:59 +0530

    added vercel analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react";
 import AppRoutes from "./app/routes/AppRoutes";
 import PageLoader from "./components/common/PageLoader";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => (
   <Suspense fallback={<PageLoader />}>
     <AppRoutes />
+    <Analytics/>
   </Suspense>
 );
 
